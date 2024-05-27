de3c8e9fd315a17f8c71c12e119c359db133463b
minayekta <mina.qolizadeh@hotmail.com>
2024-05-27 12:35:09 -0700

    add mongo

--- src/app/api/post/route.js
+++ src/app/api/post/route.js
@@ -1,3 +1,17 @@
-export default async function GET(req, res) {
+import { connectToDatabase } from '../../../utils/dbConnect'
+export async function GET(req, res) {
   res.status(200).json({ message: 'welcome' })
+}
+
+export async function POST(req, res) {
+  try {
+    const { content } = await req.json()
+    if (content.trim().length === 0) {
+      throw new error('محتوا نباید خالی باشه')
+    }
+    const db = await connectToDatabase()
+    db.collection('posts').insertOne({ content, createdAt: new Date() })
+  } catch (err) {
+    res.status(400).json(err.message)
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
-import PostCard from "@/components/PostCard";
+'use client'
+
+import PostCard from '@/components/PostCard'
+import { useState } from 'react'
 
 export default function Home() {
+  const [inputValue, setInputValue] = useState('')
+
+  const handleChange = (e: any) => {
+    setInputValue(e.target.value)
+  }
+  const handleSubmit = async (e: any) => {
+    e.preventDefault()
+    const response = await fetch('http://localhost:3000/api/post', {
+      method: 'POST',
+      body: JSON.stringify(inputValue),
+      headers: { 'Content-Type': 'application/json' },
+    })
+    const res = await response.json()
+    if (!response.ok) {
+      console.error(res)
+    } else {
+      alert('با موفقیت ایجاد شد')
+    }
+  }
+
   return (
-  <>
-        <PostCard/>
-  </>
-  );
+    <>
+      <PostCard />
+      <form onSubmit={handleSubmit} className="space-y-5">
+        <textarea className="" value={inputValue} onChange={handleChange} />
+        <button>submit</button>
+      </form>
+    </>
+  )
 }
